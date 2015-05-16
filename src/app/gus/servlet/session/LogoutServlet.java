fc65c06c46300772075b1303307f20365a5fb2f8
AlejandroBustamante <guatebus@gmail.com>
2015-05-16 18:11:44 +0200

    partial

--- src/app/gus/servlet/session/LogoutServlet.java
+++ src/app/gus/servlet/session/LogoutServlet.java
@@ -18,7 +18,7 @@ public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 3912794184499010591L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
+		response.setContentType("text/html");
         Cookie[] cookies = request.getCookies();
         if(cookies != null){
         for(Cookie cookie : cookies){
