bbad8a4f3247bd3e298e53623bbae6fa87ff7273
AlejandroBustamante <guatebus@gmail.com>
2015-05-17 16:12:37 +0200

    Added authentication filter for all requests

--- src/app/gus/servlet/filter/AuthenticationFilter.java
+++ src/app/gus/servlet/filter/AuthenticationFilter.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,11 +15,7 @@ import javax.servlet.http.HttpSession;
 @WebFilter("/AuthenticationFilter")
 public class AuthenticationFilter implements Filter {
  
-    private ServletContext context;
-     
     public void init(FilterConfig fConfig) throws ServletException {
-        this.context = fConfig.getServletContext();
-        this.context.log("AuthenticationFilter initialized");
     }
      
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -29,15 +24,12 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse res = (HttpServletResponse) response;
          
         String uri = req.getRequestURI();
-        this.context.log("Requested Resource::"+uri);
          
         HttpSession session = req.getSession(false);
          
         if(session == null && !(uri.endsWith("html") || uri.endsWith("LoginServlet"))){
-            this.context.log("Unauthorized access request");
             res.sendRedirect("login.html");
         }else{
-            // pass the request along the filter chain
             chain.doFilter(request, response);
         }
     }
